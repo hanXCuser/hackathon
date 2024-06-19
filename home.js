b314d5b4558f614ae4dd35fd9c6422c1fbfbea5c
hanXCuser <haniaimamally@gmail.com>
2024-06-19 22:28:21 +0400

    hanhan

---
+++ home.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $(document).on("click", ".super-button", function() {
+//       $(".main, .half, .overlay, .button-line, .super-button, .content").addClass("active");
+//       setTimeout(function() {
+//         $(".home *").css("z-index", "1");
+//       }, 3000);
+//     });
+//   });
+
+function change_page(){
+    document.location.href = "/play.html";
+}
---
+++ play.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('play');
+
+        button.addEventListener('click', () => {
+            window.location.href = 'play.html';
+        })
